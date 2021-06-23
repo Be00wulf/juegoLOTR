650323b0276a56c7a994c6a63c73b56af8aea1f2
María José Baquiax <mariabaquiax201831422@cunoc.edu.gt>
2021-06-23 15:04:49 -0600

    SE HA MODIFICADO LA IMPRESION DEL JEUGO

--- src/juegolotr/principal/Principal.java
+++ src/juegolotr/principal/Principal.java
@@ -1,9 +1,12 @@
 package juegolotr.principal;
 
+import juegolotr.game.Game;
+
 public class Principal {
 
     public static void main(String[] args) {
-        
+        Game g1 = new Game();
+        g1.horaDeJugar();
         
     }
     
